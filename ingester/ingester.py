23b120cc2d29c2587fa7283d349754e552747a77
drzinks <you@example.com>
2024-11-23 20:18:42 +0100

    fetching weather data for location def in config, storing to json

--- ingester/ingester.py
+++ ingester/ingester.py
@@ -1,9 +1,59 @@
-import credentials, requests, pprint
-COMPANY_SYMBOL = 'AAPL'
-URL = (f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY'
-       f'&symbol={COMPANY_SYMBOL}'
-       f'&interval=1min&apikey={credentials.api_key}')
+import requests, yaml, json, logging
 
-response = requests.get(URL)
-data = response.json()
-pprint.pprint(data)
+
+def get_logger():
+    logger = logging.getLogger("ingester_logger")
+    logger.setLevel(logging.INFO)
+
+    regular_handler = logging.FileHandler("regular.log")
+    error_handler = logging.FileHandler("error.log")
+
+    regular_handler.setLevel(logging.INFO)
+    error_handler.setLevel(logging.ERROR)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    regular_handler.setFormatter(formatter)
+    error_handler.setFormatter(formatter)
+
+    logger.addHandler(regular_handler)
+    logger.addHandler(error_handler)
+    return logger
+
+
+def load_config():
+    try:
+        with open("config/ingester-config.yaml", "r", encoding="UTF-8") as config_file:
+            return yaml.safe_load(config_file)
+    except FileNotFoundError:
+        logger.error("File does not exist.")
+
+
+def request_data():
+    global response
+    api_url = config["base_api_url"]
+    position = config["position"]
+    params = {
+        "lat": position[0],
+        "lon": position[1],
+        "altitude": config["altitude"]
+    }
+    headers = {
+        "User-Agent": config["user-agent"]
+    }
+    response = requests.get(api_url, params=params, headers=headers)
+
+
+logger = get_logger()
+config = load_config()
+
+try:
+    request_data()
+    response.raise_for_status()  # Raise an error for HTTP codes >= 400
+    data = response.json()
+    with open("weather_data.json", "w") as json_file:
+        json.dump(data, json_file, indent=4)
+        logger.info("Weather data fetched successfully.")
+except requests.exceptions.RequestException as e:
+    logger.error(f"HTTP Request failed: {e}")
+except json.JSONDecodeError:
+    logger.error("Failed to parse JSON response")
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
+### Weather info is fetched from yr.no portal, under Creative Commons Attribution 4.0 International Public License
+### https://developer.yr.no/doc/License/
 ## Ingester
 - install libraries 
 ```sh
- pip install requests
+ pip install requests, pyyaml
 ```
