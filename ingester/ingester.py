89f87a071e787b74074885598982d91e165c89a5
drzinks <you@example.com>
2024-12-02 18:08:24 +0100

    wip4

--- ingester/ingester.py
+++ ingester/ingester.py
@@ -3,20 +3,27 @@ import yaml
 import json
 import logging
 from datetime import datetime
-from requests import Response
+from kafka import KafkaProducer
+import time
+import schedule
 
 ERROR_LOG_PATH = "logs/error.log"
 LOG_PATH = "logs/regular.log"
 LOGGING_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 DAY_MONTH_YEAR_FORMAT = "%d-%m-%Y"
 YR_NO_DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
-
+KAFKA_TOPIC = "weather"
+KAFKA_BROKER = "localhost:9092"
 
 class WeatherIngester:
 
     def __init__(self):
         self.logger = self.get_logger()
         self.config = self.load_config()
+        self.producer = KafkaProducer(
+            bootstrap_servers=KAFKA_BROKER,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8")
+        )
 
     def get_logger(self):
         """Sets up logging for the application."""
@@ -45,7 +52,7 @@ class WeatherIngester:
             self.logger.error("Config file does not exist.")
             raise ValueError("Configuration file is missing.")
 
-    def request_data(self) -> Response:
+    def request_data(self) -> requests.Response:
         """Makes an HTTP request to fetch data."""
         api_url = self.config.get("base_api_url", "")
         position = self.config.get("position", [0, 0])
@@ -62,15 +69,27 @@ class WeatherIngester:
     def extract_day_night_temperatures(self, data: dict):
         """Extracts day/night temperature data from the API response."""
         day_hour_temp = []
-        for time_serie in data["properties"]["timeseries"]:
-            json_date = time_serie["time"]
-            date = datetime.strptime(json_date, YR_NO_DATE_FORMAT)
-            day_month_year = date.strftime(DAY_MONTH_YEAR_FORMAT)
-            hour = date.strftime("%H")
-            temp = time_serie["data"]["instant"]["details"]["air_temperature"]
-            if hour == self.config["hours_wanted"][0] or hour == self.config["hours_wanted"][1]:
-                day_hour_temp.append({"day": day_month_year, "hour": hour, "temp": temp})
+        try:
+            for time_serie in data["properties"]["timeseries"]:
+                hours_wanted = self.config.get("hours_wanted", ["00", "12"])
+                json_date = time_serie["time"]
+                date = datetime.strptime(json_date, YR_NO_DATE_FORMAT)
+                day_month_year = date.strftime(DAY_MONTH_YEAR_FORMAT)
+                hour = date.strftime("%H")
+                temp = time_serie["data"]["instant"]["details"]["air_temperature"]
+                if hour in hours_wanted:
+                    day_hour_temp.append({"day": day_month_year, "hour": hour, "temp": temp})
+        except KeyError as e:
+            self.logger.error(f"Missing key {e} in record: {time_serie}")
         return day_hour_temp
+
+    def send_to_kafka(self, data):
+        """Sends data to the Kafka topic."""
+        try:
+            self.producer.send(KAFKA_TOPIC, data)
+            self.logger.info("Weather data sent to Kafka.")
+        except Exception as e:
+            self.logger.error(f"Failed to send data to Kafka: {e}")
 
     def process_and_store_weather_data(self):
         """Main function to fetch, process, and store weather data."""
@@ -85,17 +104,29 @@ class WeatherIngester:
                 if so this should be logged and/or shown as a warning. Deprecated versions will be terminated after a reasonable time period (usually 1 month).
                 """
             data = response.json()
+            self.logger.info("Weather data fetched successfully.")
             short_data = self.extract_day_night_temperatures(data)
-            # Write data to a local JSON file
-            with open("processed_weather_data.json", "w") as json_file:
-                json.dump(short_data, json_file, indent=4)
-                self.logger.info("Weather data fetched successfully.")
+            self.logger.info("Weather data sent to kafka.")
+            self.send_to_kafka(data)
+            # with open("processed_weather_data.json", "w") as json_file:
+            #     json.dump(short_data, json_file, indent=4)
+            #     self.logger.info("Weather data fetched successfully.")
         except requests.exceptions.RequestException as e:
             self.logger.error(f"HTTP Request failed: {e}")
         except json.JSONDecodeError:
             self.logger.error("Failed to parse JSON response")
 
 
-if __name__ == "__main__":
+def scheduled_job():
     ingester = WeatherIngester()
     ingester.process_and_store_weather_data()
+
+if __name__ == "__main__":
+    schedule.every().day.at("00:01").do(scheduled_job)
+    schedule.every().day.at("12:01").do(scheduled_job)
+    scheduled_job() #first run at once
+    print("Weather ingestion service started. Press Ctrl+C to stop.")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
--- readme.md
+++ readme.md
@@ -3,5 +3,32 @@
 ## Ingester
 - install libraries 
 ```sh
- pip install requests, pyyaml, pytest
+ pip install requests pyyaml pytest kafka-python-ng schedule
+```
+- run ingester
+```sh
+python weather_ingester.py
+```
+
+## Kafka
+- install Podman & podman-compose
+```sh
+podman machine init
+podman machine start
+pip install podman-compose
+```
+- run podman compose
+```sh
+podman-compose up -d
+```
+- setup kafka topic
+```sh
+podman exec -it kafka bash
+kafka-topics.sh --create --topic weather --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+```
+## Postgresql
+- download and run postgresql on podman
+```
+podman pull docker.io/postgres:latest
+podman run --name postgres -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -p 5432:5432 -v /c/projects/postgress-podman -d postgres
 ```
